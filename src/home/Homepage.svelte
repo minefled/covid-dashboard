8e7d6c7dfb86422af6948bbf16aef62123984dab
minefled <florian-martin@stembal.de>
2021-11-25 17:53:41 +0100

    Added Navigator to Home page

--- src/home/Homepage.svelte
+++ src/home/Homepage.svelte
@@ -1,10 +1,41 @@
 <script>
-
 </script>
 
 <div class="home-page">
     <div class="landing-banner">
         <b class="info-message">Die aktuellen Covid-19 Zahlen und Statistiken aus Deutschland</b>
+    </div>
+
+    <div class="quick-navigator">
+        <div class="header">
+            <b class="title">Navigator</b>
+        </div>
+
+        <div class="item-list">
+            <div class="item" on:click={() => { window.location.href = "#world"; }}>
+                <div class="item-box">
+                    <b class="item-name">Welt</b>
+                </div>
+            </div>
+
+            <div class="item" on:click={() => { window.location.href = "#germany"; }}>
+                <div class="item-box">
+                    <b class="item-name">Deutschland</b>
+                </div>
+            </div>
+
+            <div class="item" on:click={() => { window.location.href = "#states"; }}>
+                <div class="item-box">
+                    <b class="item-name">Bundesländer</b>
+                </div>
+            </div>
+
+            <div class="item" on:click={() => { window.location.href = "#districts"; }}>
+                <div class="item-box">
+                    <b class="item-name">Landkreise</b>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
