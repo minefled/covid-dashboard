c60b4513ca8ca762362341d36f38648f223d18ad
minefled <florian-martin@stembal.de>
2021-11-26 16:40:46 +0100

    Added world, states and districts pages to core/pages.ts

--- public/assets/core/page.ts
+++ public/assets/core/page.ts
@@ -1,9 +1,18 @@
-export type Page = "home" | "germany";
+export type Page = "home" | "world" | "germany" | "states" | "districts";
 
 export function get_page():Page {
     switch(window.location.hash) {
+        case "#world":
+            return "world";
+
         case "#germany":
             return "germany";
+
+        case "#states":
+            return "states";
+
+        case "#districts":
+            return "districts";
 
         default:
             return "home";
